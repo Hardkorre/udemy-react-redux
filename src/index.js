09bd3ffbf7b972583622d60b22096ff2b4fa255d
Grant Winman <gwinman@definitivelogic.com>
2018-11-03 12:08:27 -0600

    Module-10-a complete.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyD1DxgSBROIJKWBNnbg7fUTKzzvt48FYvI';
 
 // Create a new component, this component should create HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it in the DOM
