8a3104aff52372a8935c1cfb962ece648055c4b2
= <gwinman@definitivelogic.com>
2018-11-03 15:24:06 -0600

    Saving progress.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD1DxgSBROIJKWBNnbg7fUTKzzvt48FYvI';
 
-// Create a new component, this component should create HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+class App extends Component {
+
+    constructor(props){
+        
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'beekeeping'}, (videos) => {
+            this.setState({videos});
+        });
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+
 }
 
-// Take this component's generated HTML and put it in the DOM
  ReactDOM.render(<App />, document.querySelector('.container'));
