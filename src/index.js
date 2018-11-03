68380ad0781d407cf8b16d2bfa0a264b58092de4
Grant Winman <gwinman@definitivelogic.com>
2018-11-03 11:05:46 -0600

    Pushing progress on setting up components.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyD1DxgSBROIJKWBNnbg7fUTKzzvt48FYvI';
+
 // Create a new component, this component should create HTML
-const App = function() {
+const App = () => {
     return <div>Hi!</div>;
 }
 
 // Take this component's generated HTML and put it in the DOM
+ ReactDOM.render(<App />, document.querySelector('.container'));
