c62555dfe7ffcb3176673b4d153c2478f8ed2c33
Grant Winman <gwinman@definitivelogic.com>
2018-12-03 21:18:33 -0700

    Progress on Section 16.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SearchBar from './components/search_bar';
+import App from './components/App';
 
-const API_KEY = 'AIzaSyD1DxgSBROIJKWBNnbg7fUTKzzvt48FYvI';
-
-// Create a new component, this component should create HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-}
-
-// Take this component's generated HTML and put it in the DOM
- ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
